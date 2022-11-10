7acc0ec4f62ee07943967c81a3b389ce19689010
Phan Vu <npvu19@clc.fitus.edu.vn>
2022-11-10 17:16:34 +0700

    submit

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Gallery from "./Gallery";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const getPosts = async () => {
   const response = await fetch("https://api.imgflip.com/get_memes");
@@ -42,10 +42,25 @@ function App() {
   return (
     <div className="App">
       <QueryClientProvider client={client}>
-        <Container>
-          {button}
-          <Gallery flag={flag}></Gallery>
-        </Container>
+        {/*         <Container>
+          <Row className="justify-content-md-center">
+            <Col>{button}</Col>
+          </Row>
+
+          <Row>
+            <Gallery flag={flag}></Gallery>
+          </Row>
+        </Container> */}
+        <Row className="justify-content-md-center">
+          <Col xs lg="7">
+            {button}
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col xs lg="7">
+            <Gallery flag={flag}></Gallery>
+          </Col>
+        </Row>
       </QueryClientProvider>
     </div>
   );
